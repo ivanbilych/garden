f8e971e58d8b7f1bf02b9d4c2993b504c6471bae
Ivan Bilych <ivanbilych@gmail.com>
2019-07-04 12:44:01 -0400

    Add command line arguments parser
    Signed-off-by: Ivan Bilych <ivanbilych@gmail.com>

--- garden.py
+++ garden.py
@@ -1,7 +1,31 @@
 #!/usr/bin/python3
 
+import argparse
+
+client_ip = None
+client_port = None
+server_port = None
+verbosity = False
+
+def parse_command_line_arguments():
+    global client_ip, client_port, server_port, verbosity
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-v", "--verbose", help="print debug information", action="store_true")
+    parser.add_argument("client_ip", help="client IP address")
+    parser.add_argument("client_port", help="client port", type=int)
+    parser.add_argument("server_port", help="server port", type=int)
+
+    args = parser.parse_args()
+
+    client_ip = args.client_ip
+    client_port = args.client_port
+    server_port = args.server_port
+    verbosity = args.verbose
+
 def main():
-    print("python main function")
+    parse_command_line_arguments()
 
 if __name__ == "__main__":
     main()
