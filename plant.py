2d2b356eb2ab23f00bd47ed6878d177286e38916
Ivan Bilych <ivanbilych@gmail.com>
2019-07-13 14:19:11 -0400

    fix plant
    Signed-off-by: Ivan Bilych <ivanbilych@gmail.com>

--- plant.py
+++ plant.py
@@ -1,4 +1,7 @@
 import datetime
+import json
+
+from mgprint import gprint
 
 class Plant():
     def __init__(self, name, value, water, frequency, grow_time):
@@ -7,20 +10,22 @@ class Plant():
         self.WATER = water
         self.FREQUENCY = frequency
         self.GROW_TIME = grow_time
+        self.REQUIRED_WATER = water/frequency
 
         self.is_alive = True
         self.grow_up = False
 
         self.plant_time = datetime.datetime.today()
 
-    def drink_water(seld, water):
-        gprint("plant %s drinks %d of water" % (self.NAME, self.WATER))
-        if water < self.WATER:
+    def drink_water(self, water):
+        gprint("plant %s drinks %d of water" % (self.NAME, water))
+        if water < self.REQUIRED_WATER:
             gprint("plant %s DIES" % self.NAME)
             self.is_alive = False
 
     def update_status(self):
-        if self.grow_up:
+        gprint("plant %s: ALIVE - %s, GROW UP - %s" % (self.NAME, self.is_alive, self.grow_up))
+        if self.grow_up or not self.is_alive:
             return
 
         timedelta = datetime.datetime.today() - self.plant_time
@@ -28,3 +33,6 @@ class Plant():
         if timedelta.seconds > self.GROW_TIME:
             gprint("plant %s GROW UP" % self.NAME)
             self.grow_up = True
+
+def json_to_plant(json_pkg):
+    return Plant(json_pkg["name"], json_pkg["value"], json_pkg["water"], json_pkg["frequency"], json_pkg["grow_time"])
