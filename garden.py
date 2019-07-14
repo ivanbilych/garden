53935578cc14117f69f63cb4dd03a6fb4198151c
Ivan Bilych <ivanbilych@gmail.com>
2019-07-14 12:10:45 -0400

    Move nature to separate module
    Signed-off-by: Ivan Bilych <ivanbilych@gmail.com>

--- garden.py
+++ garden.py
@@ -1,50 +1,14 @@
-import time
-import threading
-
 import growplace
 from mgprint import gprint
+import nature
 import plant
 
 class Garden():
     grow_places = []
 
-    class Nature(threading.Thread):
-        def __init__(self, grow_places):
-            threading.Thread.__init__(self)
-            self.stop_nature = False
-            self.no_more_plants = False
-            self.grow_places = grow_places
-
-        def check_plants(self):
-            if self.no_more_plants:
-                self.stop_nature = True
-
-            for place in self.grow_places:
-                if not place.plant.grow_up and place.plant.is_alive:
-                    self.stop_nature = False
-                place.plant.update_status()
-
-        def drink_water(self):
-            for place in self.grow_places:
-                if place.plant.is_alive:
-                    water = place.gave_water(place.plant.REQUIRED_WATER)
-                    gprint(" we have %.2f water for %s (%.2f left)" % (water, place.plant.NAME, place.water))
-                    place.plant.drink_water(water)
-
-        def run(self):
-            while not self.stop_nature:
-                self.drink_water()
-                time.sleep(1)
-                self.check_plants()
-
-            gprint(" no more plants to grow")
-
-        def stop(self):
-            self.no_more_plants = True
-
     def __init__(self, water):
         self.water_tank = float(water)
-        self.nature_thread = self.Nature(self.grow_places)
+        self.nature_thread = nature.Nature(self.grow_places)
         gprint(" garden tank have %.2f of water" % water)
 
     def start(self):
